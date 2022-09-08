98aabdb59ab4cd9f7ad0a0ba611238fe402270cf
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2022-09-08 23:39:55 +0200

    add some reusable components

---
+++ lib/shared/component/component.dart
@@ -0,0 +1,156 @@
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, avoid_types_as_parameter_names, non_constant_identifier_names, sort_child_properties_last
+
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
+import 'package:flutter/material.dart';
+
+Widget defaultButton({
+  double width = double.infinity,
+  double radius = 0.0,
+  required Color background,
+  required Function? function,
+  required String text,
+}) =>
+    Container(
+      width: width,
+      height: 40,
+      child: MaterialButton(
+        onPressed: () {
+          function!();
+        },
+        child: Text(
+          text.toUpperCase(),
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(radius),
+        color: background,
+      ),
+    );
+
+Widget defaultFormField({
+  required TextEditingController controller,
+  required TextInputType type,
+  required String? Function(String? val)? validate,
+  required String label,
+  required IconData prefix,
+  Function? onTap,
+  bool isClickable = true,
+  bool isPassword = false,
+  IconData? suffix,
+  Function? suffixPressed,
+  Function(String value)? onFieldSubmitted,
+  Function(String value)? onChanged,
+}) =>
+    TextFormField(
+      controller: controller,
+      keyboardType: type,
+      obscureText: isPassword,
+      onFieldSubmitted: (String value) {
+        onFieldSubmitted!(value);
+      },
+      onChanged: (String value) {
+        onChanged!(value);
+      },
+      onTap: () {
+        onTap!();
+      },
+      enabled: isClickable,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Icon(
+          prefix,
+        ),
+        suffixIcon: suffix != null
+            ? IconButton(
+                onPressed: () {
+                  suffixPressed!();
+                },
+                icon: Icon(
+                  suffix,
+                ),
+              )
+            : null,
+        border: const OutlineInputBorder(),
+      ),
+      validator: validate,
+    );
+
+Widget buildArticleItem(article, context) => Padding(
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        children: [
+          Container(
+            width: 120,
+            height: 120,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: NetworkImage('${article['urlToImage']}'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Expanded(
+            child: Container(
+              height: 120,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  Expanded(
+                    child: Text(
+                      '${article['title']}',
+                      style: Theme.of(context).textTheme.bodyText1,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Text(
+                    '${article['publishedAt']}',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+
+Widget myDivider() => Padding(
+      padding: const EdgeInsetsDirectional.only(
+        start: 20.0,
+      ),
+      child: Container(
+        width: double.infinity,
+        height: 10,
+        color: Colors.grey[300],
+      ),
+    );
+
+Widget articleBuilder(list, context, {isSearch = false}) => ConditionalBuilder(
+    condition: list.length > 0,
+    builder: ((context) => ListView.separated(
+        physics: BouncingScrollPhysics(),
+        itemBuilder: (context, index) => buildArticleItem(list[index], context),
+        separatorBuilder: (context, index) => myDivider(),
+        itemCount: 10)),
+    fallback: ((context) =>
+        isSearch ? Container() : Center(child: CircularProgressIndicator())));
+
+void navigateTo(context, Widget) => Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Widget,
+      ),
+    );
